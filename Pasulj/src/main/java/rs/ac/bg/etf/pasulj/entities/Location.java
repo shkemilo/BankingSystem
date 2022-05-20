6de6324c6710f3189844c5f075d4dd0c3745609c
Mateja Milosevic <mateja.milosevickg99@gmail.com>
2022-05-20 22:19:30 +0200

    Inital commit

---
+++ Pasulj/src/main/java/rs/ac/bg/etf/pasulj/entities/Location.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.etf.pasulj.entities;
+package javax.per
+
+/**
+ *
+ * @author matej
+ */
+@Entiy
+public class Location {
+    
+}
