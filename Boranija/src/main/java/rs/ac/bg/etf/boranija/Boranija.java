ffea22298b1cd915dca3fcf8728d6ce4e4eb2a78
Mateja Milosevic <mateja.milosevickg99@gmail.com>
2022-05-26 23:33:24 +0200

    Updated projects to use Glassfish server for hosting

--- Boranija/src/main/java/rs/ac/bg/etf/boranija/Boranija.java
+++ Boranija/src/main/java/rs/ac/bg/etf/boranija/Boranija.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rs.ac.bg.etf.boranija.boranija;
+package rs.ac.bg.etf.boranija;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
