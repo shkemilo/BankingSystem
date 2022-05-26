f010b768550d098da73fc40efa2c6ec453cf37b9
Mateja Milosevic <mateja.milosevickg99@gmail.com>
2022-05-26 02:15:40 +0200

    Implement DataSenderController for Boranija

---
+++ Boranija/src/main/java/rs/ac/bg/etf/boranija/controllers/DataSenderController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.etf.boranija.controllers;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import rs.ac.bg.etf.boranija.entities.EntityUtility;
+
+/**
+ *
+ * @author matej
+ */
+public class DataSenderController {
+    EntityManagerFactory emf;
+    
+    public DataSenderController() {
+        emf = Persistence.createEntityManagerFactory("kupusPU");
+    }
+    
+    public <T> List<T> getAll(Class<T> entityClass) {   
+        EntityManager entityManager = emf.createEntityManager();
+        
+        if(!EntityUtility.isEntity(entityManager, entityClass)) {
+            entityManager.close();
+            return null;
+        }
+        
+        TypedQuery<T> query = entityManager.createQuery("SELECT e FROM " + entityClass.getSimpleName(), entityClass);
+        List<T> entities = query.getResultList();
+        
+        entityManager.close();
+        
+        return entities;
+    }
+    
+    @Override
+    protected void finalize() {
+        emf.close();
+    }
+}
---
+++ Boranija/src/main/java/rs/ac/bg/etf/boranija/entities/EntityUtility.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.etf.boranija.entities;
+
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+/**
+ *
+ * @author matej
+ */
+public class EntityUtility {
+    
+    public static boolean isEntity(EntityManager entityManager, Class<?> clazz) {
+        boolean foundEntity = false;
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        for(EntityType<?> entityType : entities) {
+            Class<?> entityClass = entityType.getJavaType();
+            if(entityClass.equals(clazz)) {
+                foundEntity = true;
+            }
+        }
+        
+        return foundEntity;
+    }
+}
