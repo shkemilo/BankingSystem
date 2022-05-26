78cd10d8a79bc9bd0dac04e282d7de65c45c3b77
Mateja Milosevic <mateja.milosevickg99@gmail.com>
2022-05-26 23:41:43 +0200

    Migrat controller classes.

--- Kupus/src/main/java/rs/ac/bg/etf/kupus/utils/EntityUtility.java
+++ Kupus/src/main/java/rs/ac/bg/etf/kupus/utils/EntityUtility.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rs.ac.bg.etf.kupus.entities;
+package rs.ac.bg.etf.kupus.utils;
 
 import java.util.List;
 import java.util.Set;
