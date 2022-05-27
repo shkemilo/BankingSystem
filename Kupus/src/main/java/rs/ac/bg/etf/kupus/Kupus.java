b111fb3dccaaa125c2ca3f101bd57b1b6affddae
Mateja Milosevic <mateja.milosevickg99@gmail.com>
2022-05-27 02:26:31 +0200

    Implement startup listener on kupus. Starts a periodical task in 2 minute intervals.

--- Kupus/src/main/java/rs/ac/bg/etf/kupus/Kupus.java
+++ Kupus/src/main/java/rs/ac/bg/etf/kupus/Kupus.java
@@ -5,41 +5,14 @@
  */
 package rs.ac.bg.etf.kupus;
 
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import rs.ac.bg.etf.commons.controllers.DataSyncController;
-import rs.ac.bg.etf.kupus.entities.Client;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
  * @author matej
  */
 public class Kupus {
-    public static void main(String[] args) {
-        System.out.println("Kupus starting!");
-        
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("kupusPU");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
-        entityManager.getTransaction().begin();
-        
-        Client client = new Client("Marko Markovic", "LOL 123", null);
-        List<Client> clients = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            clients.add(new Client("Mile" + i, "LOL 123", null));
-        }
-        
-        DataSyncController dsc = new DataSyncController(entityManager);
-        dsc.sync(clients, Client.class);
-        
-        entityManager.getTransaction().commit();
-        
-        entityManager.close();
-        entityManagerFactory.close();
-        
-        System.out.println("Kupus ending!");
-    }
+
 }
---
+++ Kupus/src/main/java/rs/ac/bg/etf/kupus/KupusInitializeServlet.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.etf.kupus;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ *
+ * @author matej
+ */
+@WebListener
+public class KupusInitializeServlet implements ServletContextListener {
+    
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("Kupus is starting");
+        Runnable syncTask = () -> {
+            logger.info("Sync statring");
+        };
+        
+        ScheduledExecutorService periodicSyncExecutor = Executors.newScheduledThreadPool(1);
+        periodicSyncExecutor.scheduleAtFixedRate(syncTask, 0, 2, TimeUnit.MINUTES);
+    }
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        logger.info("Kupus is shutting down!");
+    }
+}
